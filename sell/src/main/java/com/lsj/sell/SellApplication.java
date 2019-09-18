bbaadae172c9860846771cc2c71c071907229560
hdb001 <hdb001@ecaicn.com>
2019-09-18 11:28:52 +0800

    调整项目结构

--- sell/src/main/java/com/lsj/sell/SellApplication.java
+++ sell/src/main/java/com/lsj/sell/SellApplication.java
@@ -1,5 +1,7 @@
 package com.lsj.sell;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,9 +10,11 @@ import java.util.Date;
 @SpringBootApplication
 public class SellApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SellApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SellApplication.class, args);
-        System.out.println("[启动成功]"+new Date());
+        LOGGER.debug("[启动成功]" + new Date());
     }
 
 }
